84db2c5689aa8abde841ab2b4f728400041a2e28
John N. Norman <normanj777@gmail.com>
2021-02-15 08:38:26 -0500

    add email verification script

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const error = document.getElementById('email-error')
 
-let emailIsValid = false;
+const isEmail = (email)=> {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 
-const emailError = ()=> {
-    if (!emailIsValid) {
-        error.classList.add("error-active")
-    } else {
+const validate = () => {
+    const inputEmail = document.getElementById('email').value 
+    if (isEmail(inputEmail)) {
         error.classList.remove('error-active')
+    } else {
+        error.classList.add('error-active')
+
     }
 }
 
-emailError()
+document.getElementById('submit').addEventListener( 'click', (e)=> {
+    e.preventDefault()
+    validate()
+})
